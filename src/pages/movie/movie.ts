d10c486045cb95fc34eddce51bb6e4ea6436d86d
tanatat01 <tanatat253919@gmail.com>
2019-03-23 11:36:09 +0700

    up

--- src/pages/movie/movie.ts
+++ src/pages/movie/movie.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Http } from '@angular/http'
+import 'rxjs/add/operator/map';
 /**
  * Generated class for the MoviePage page.
  *
@@ -15,7 +16,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MoviePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  movnews:any=0;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http:Http) {
+    this.http.get('https://www.reddit.com/r/popular/new/.json?limit=20').map(res => res.json()).subscribe(data =>
+    {
+        this.movnews = data.data.children;
+    });
   }
 
   ionViewDidLoad() {
